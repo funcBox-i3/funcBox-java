7050d058a97e16f7938c36a93cb23271d546c2ae
kishore P <kishore42835@gmail.com>
2025-05-22 22:56:34 +0530

    commit - 1.0.2
    - Factors
    - Fibonacci

--- src/main/java/funcBox/funcBox.java
+++ src/main/java/funcBox/funcBox.java
@@ -3,7 +3,7 @@ package funcBox;
 
 public class funcBox {
 
-    // Prime function
+    //Prime
     public boolean isPrime(int num) {
         if (num <= 1) {
             return false;
@@ -19,13 +19,38 @@ public class funcBox {
     }
 
     //Palindrome
-    public boolean isPalindrome(String val){
+    public boolean isPalindrome(String val) {
         val = val.toLowerCase();
-        StringBuilder pal= new StringBuilder();
-        for (int i = val.length()-1;i>=0;i--) {
+        StringBuilder pal = new StringBuilder();
+        for (int i = val.length() - 1; i >= 0; i--) {
             pal.append(val.charAt(i));
         }
         return pal.toString().equals(val);
+    }
+
+    //Factors
+    public String getFactors(int num) {
+        if (num <= 0) return "[]";
+        java.util.List<Integer> factors = new java.util.ArrayList<>();
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0 && i != num) {
+                factors.add(i);
+            }
+        }
+        return factors.toString();
+    }
+
+    //Fibonacci
+    public int fibonacci(int num) {
+        if (num <= 1) return num;
+        int fib = 1;
+        int prevFib = 1;
+        for (int i = 2; i <= num; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
     }
 
 }
